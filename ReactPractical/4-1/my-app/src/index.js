440dae75a5d21d30ceab056c46cd2ea35c2d6d2c
Yoshihiro Yamaguchi <gucchiy@gmail.com>
2023-03-23 23:45:10 +0900

    add ReactPractical 4-1

--- ReactPractical/4-1/my-app/src/index.js
+++ ReactPractical/4-1/my-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +15,10 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+import ReactDOM from "react-dom";
+import { App } from "./App";
+// import { Fragment } from "react";
+
+ReactDOM.render(<App />, document.getElementById("root"));
